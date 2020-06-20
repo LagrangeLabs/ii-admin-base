62743934c7ef05074317bc8dd7207a2d4bea5e1b
Bian2017 <bgl_cumt_zju@163.com>
2020-06-20 15:58:00 +0800

    feat: 完成组件信息展示

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,10 +1,20 @@
-import { addDecorator, configure } from '@storybook/react';
+import { addDecorator, addParameters, configure } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
 import WrapperDecorator from './decorators/WrapperDecorator';
 
 import 'antd/dist/antd.less';
 
 // 通过addDecorator添加插件
 addDecorator(WrapperDecorator);
+addDecorator(withInfo);
+
+addParameters({
+  info: {
+    inline: true, // 直接展示所有信息
+    header: false, // 隐藏组件名称和组件描述
+    source: false,
+  },
+});
 
 // 将 welcome 文档说明置于顶部
 const loaderFn = () => {
