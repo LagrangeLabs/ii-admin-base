0548d33e8035446e9620426e0da95987a93fe800
Bian2017 <bgl_cumt_zju@163.com>
2020-06-23 14:22:24 +0800

    feat: 修改部署密钥路径

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,18 +4,18 @@ set -e
 openssl version
 
 # setup ssh-agent and provide the GitHub deploy key
-openssl aes-256-cbc -k "$super_secret_password" -in config/travis/deploy.enc -out config/travis/deploy -d
+openssl aes-256-cbc -k "$super_secret_password" -in config/travis/deploy.enc -out deploy -d
 
 # 对解密后的私钥添加权限
-chmod 600 config/travis/deploy
+chmod 600 deploy
 
 # 启动 ssh-agent
 eval "$(ssh-agent -s)"
 
-ssh-add config/travis/deploy
+ssh-add deploy
 
 # 删除解密后的私钥
-rm config/travis/deploy
+rm deploy
 
 git config --global user.name 'Travis'  
 git config --global user.email 'travis@travis-ci.com' 
