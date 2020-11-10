def7d9857ee89c8abd3bc374f5450100d816a6f3
大黄蜂(张明瑞) <zhangmingrui@mail.i-i.ai>
2020-11-10 15:33:17 +0800

    feat: MyForm 组件改为业务组件

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export { default as HighlightCard } from './HighlightCard';
 export { default as InputVerify } from './InputVerify';
 export { default as SelectSearch } from './SelectSearch';
-export { default as MyForm } from './MyForm/V4';
 export { default as MyUpload } from './MyUpload';
