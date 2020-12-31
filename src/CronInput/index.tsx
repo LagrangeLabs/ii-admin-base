fe6d607957a917ccc3121705ea54ed06fb68eebb
xuli <iforeach@163.com>
2020-12-31 11:53:15 +0800

    feat: cornInput组件添加

---
+++ src/CronInput/index.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Dropdown, Input } from 'antd';
+import Cron from './cron';
+
+type TProps = {
+  style?: React.CSSProperties;
+  inputStyle?: React.CSSProperties;
+};
+
+export const CronInput: React.FC<TProps> = props => {
+  const { style, inputStyle, ...passThroughProps } = props;
+  const [value, setValue] = useState('');
+  const [visible, setVisible] = useState(false);
+  const onChange = (value: string) => {
+    setValue(value);
+    setVisible(false);
+  };
+  const handleVisibleChange = (flag: boolean) => {
+    setVisible(flag);
+  };
+  return (
+    <div>
+      <Dropdown
+        trigger={['click']}
+        placement="bottomLeft"
+        onVisibleChange={handleVisibleChange}
+        visible={visible}
+        overlay={<Cron onChange={onChange} value={value} style={style} />}
+      >
+        <Input value={value} style={inputStyle} {...passThroughProps} />
+      </Dropdown>
+    </div>
+  );
+};
+
+export default CronInput;
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export { default as IUpload } from './IUpload';
 export { default as SearchTree } from './SearchTree';
 export { default as ISelectTree } from './ISelectTree';
 export { default as CodeMirror } from './CodeMirror';
+export { default as CronInput } from './CronInput';
